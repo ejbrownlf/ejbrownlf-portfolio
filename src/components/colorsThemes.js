db3142e7082513a2e98c463ca683a12e76c3339f
ejbrownlf <ejbrownlf@gmail.com>
2021-06-02 13:55:23 -0500

    color theme

---
+++ src/components/colorsThemes.js
@@ -0,0 +1,10 @@
+const themeOne = {
+    pink: 'rgb(277.1, 85.3, 164.5)',
+    lightBlue: '#3fdceb',
+    purple: '#160d2b',
+    greenGrey: '#455f65',
+    white: '#dcd5d5',
+    darkBlue: '#212142'
+}
+
+export default themeOne;
