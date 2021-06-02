d49be83aa1bb63e4681799e0b8356faa7d711fa1
ejbrownlf <ejbrownlf@gmail.com>
2021-06-02 13:53:14 -0500

    minor projects

---
+++ src/info/githubPrj.js
@@ -0,0 +1,59 @@
+const projects = [
+    {
+        name: 'Daily Planner',
+        github: 'https://github.com/ejbrownlf/Daily-Planner',
+        tools: [ 'jQuery, ', 'Momentjs' ],
+        desc: 'simple day planner'
+    },
+    {
+        name: 'Weather Dashboard',
+        github: 'https://github.com/ejbrownlf/weather-dashboard',
+        tools: [ 'jQuery, ', 'OpenWeatherAPI' ],
+        desc: 'View weather stats for your city'
+    },
+    {
+        name: 'Coding Quiz',
+        github: 'https://github.com/ejbrownlf/coding-quiz',
+        tools: ['JavaScript' ],
+        desc: 'A little quiz app for your coding skills'
+    },
+    {
+        name: 'Password Generator',
+        github: 'https://github.com/ejbrownlf/Password-Generator',
+        tools: ['JavaScript' ],
+        desc: 'Simple Password generator'
+    },
+    {
+        name: 'README Generator',
+        github: 'https://github.com/ejbrownlf/README-Generator',
+        tools: ['Node.js, ', 'Inquirer' ],
+        desc: 'CLI program that generates a professional README.md'
+    },
+    {
+        name: 'Note Taker',
+        github: 'https://github.com/ejbrownlf/Note-Taker',
+        tools: ['Node.js, ', 'Express' ],
+        desc: 'Simple Note taking app'
+    },
+    {
+        name: 'Employee Tracker',
+        github: 'https://github.com/ejbrownlf/employee-tracker',
+        tools: ['Node.js, ', 'MySQL' ],
+        desc: 'CLI program that assists in tracking company information'
+    },
+    {
+        name: 'Workout Tracker',
+        github: 'https://github.com/ejbrownlf/workout-tracker',
+        tools: ['Node.js, ', 'Express, ', 'Heroku, ', 'MVC, ', 'MongoDB'],
+        desc: 'Live heroku app that tracks workout information'
+    },
+    {
+        name: 'Budget Tracker',
+        github: 'https://github.com/ejbrownlf/budget-tracker',
+        tools: ['Node.js, ', 'Express, ', 'Heroku, ', 'MVC, ', 'MongoDB, ', 'IndexDB' ],
+        desc: 'Live Heroku app that tracks budget information and has offline functionality'
+    },
+    
+]
+
+export default projects
