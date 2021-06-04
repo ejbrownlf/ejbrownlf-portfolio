6340486ab30440ab7e1857eff402507f7f10d1b2
ejbrownlf <ejbrownlf@gmail.com>
2021-06-03 19:20:52 -0500

    created content page

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import themeOne from "./colorsThemes"
 
-const { pink, lightBlue, white } = themeOne
+const { pink, lightBlue, white, purple } = themeOne
 
 const AboutContainer = styled.section`
   display: flex;
@@ -10,26 +10,10 @@ const AboutContainer = styled.section`
   align-items: center;
   flex-direction: column;
   margin: 0 auto;
-  min-height: 100vh;
+  min-height: 75vh;
   justify-content: center;
   color: ${white};
   max-width: 1000px;
-
-  div {
-    display: block;
-    margin: 0px;
-    margin-right: auto;
-    padding: 0px;
-  }
-
-  .aboutGrid {
-    display: grid;
-    grid-template-columns: 3fr 1fr;
-    height: 75vh;
-    width: 100%;
-    margin: 0;
-    gap: 50px;
-  }
 
   h3 {
     margin: 0 0 5px 0;
@@ -40,99 +24,48 @@ const AboutContainer = styled.section`
     display: grid;
     font-size: 18px;
   }
-`
 
-const StyledImg = styled.img`
-  width: 100%;
-  border-radius: 50%;
-`
-
-const ListGrid = styled.section`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 0px;
-  margin: 0px;
-  /* margin-top: 50px; */
-  /* border: 1px solid;
-  border-color: ${pink}; */
-`
-
-const ListGroup = styled.ul`
-  margin-right: auto;
-  list-style: none;
-  font-size: 15px;
-  color: ${lightBlue};
+  button {
+    width: 30%;
+    height: 50px;
+    color: ${lightBlue};
+    background-color: ${purple};
+    border: 1px solid;
+    border-radius: 6px;
+    border-color: ${purple};
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 10px;
+  }
+  a {
+    color: ${lightBlue};
+  }
 `
 
 const Contact = () => (
-  <AboutContainer id="about">
+  <AboutContainer id="contact">
+    <h3 style={{ paddingLeft: "20px", color: pink, marginRight: "auto" }}>
+      Talk To Me! :{" "}
+    </h3>
+
+    <p>
+      I am actively seeking oppurtunities as a Web Developer and/or Cyber
+      Security Specialist. Feel free to reach out if you are interested even if
+      its just a random web question!
+    </p>
+    <button>
+      <a
+        href="mailto:ejbrownlf@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reach Out!
+      </a>
+    </button>
     <div>
-      <h3> {"{ "}</h3>
-      <h3 style={{ paddingLeft: "20px", color: pink }}>About-Me : </h3>
-    </div>
-    <div className="aboutGrid">
-      <div>
-        <p>
-          Hello whoever may be reading this, I'm Erik! I started my interest in
-          learning more about engineering when I was in high school when decided
-          to build a trebuchet with a floating arm design for my school science
-          fair. I did very well with it and impressed my physics teacher. The
-          next year my city started a pumpkin launching competition and my
-          teacher suggested I put a team together and scale up my trebuchet
-          design for the competition. This competetion allowed me to meet
-          several people in the engineering industry and solidified my interest
-          in engineering going into college.
-        </p>
-        <p>
-          I went got accepted into Auburn University with a scholarship where I
-          spent the next five years learning more about engineering. This is
-          where I was introduced to programming through MATLAB and Excel VBA. I
-          used these languages to solve complex engineering problems including
-          differential equations and gaussian elimination to solve huge systems
-          of equations. Unfortunately due Covid-19 and life circumstances I had
-          to step away from college.
-        </p>
-        <p>
-          During my break I was trying to figure out what to do next and I
-          really wanted to do more with programming and I became very interested
-          in Web Development. This led me the the Coding Bootcamp at UNC for
-          Fullstack Web Development. I enrolled and earned my certificate on May
-          19th, 2021. Here is a list of some of the skills I've learned through
-          this bootcamp and on my own:
-        </p>
-      </div>
-      <div>
-        <StyledImg src="assets/unnamed.jpg"></StyledImg>
-        Tools:
-        <ListGrid>
-          <ListGroup>
-            <li>React</li>
-            <li>Node.js</li>
-            <li>MongoDB</li>
-          </ListGroup>
-          <ListGroup>
-            <li>MySQL</li>
-            <li>Django</li>
-            <li>Firebase</li>
-          </ListGroup>
-        </ListGrid>
-        Languages:
-        <ListGrid>
-          <ListGroup>
-            <li>HTML5</li>
-            <li>CSS3</li>
-            <li>JavaScript</li>
-          </ListGroup>
-          <ListGroup>
-            <li>Python3</li>
-            <li>Java</li>
-            <li>SQL</li>
-          </ListGroup>
-        </ListGrid>
-      </div>
+      
     </div>
   </AboutContainer>
 )
 
-export default Contact;
+export default Contact
