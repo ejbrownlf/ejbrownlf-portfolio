40da9a66a5b4964c4dac69863efa958d2e30caf1
ejbrownlf <ejbrownlf@gmail.com>
2021-06-03 19:19:28 -0500

    added links and mobile design

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,12 @@
 import React from "react"
 import styled from "styled-components"
 import themeOne from "./colorsThemes"
+import laughs from "../assets/Laughs-Eats-Screenshot.png"
+import posts from "../assets/postsnstuff.png"
+import mentorme from "../assets/landingpage.png";
+import useWindowDimensions from "../hooks/windowSize";
 
-
-const { pink, lightBlue, white } = themeOne
+const { pink, white, darkBlue } = themeOne
 
 const ProjectContainer = styled.section`
   display: flex;
@@ -11,7 +14,7 @@ const ProjectContainer = styled.section`
   align-items: center;
   flex-direction: column;
   margin: 0 auto;
-  min-height: 150vh;
+  min-height: 130vh;
   justify-content: center;
   color: ${white};
   max-width: 1000px;
@@ -26,10 +29,14 @@ const ProjectContainer = styled.section`
   .groupPGrid {
     display: grid;
     grid-template-columns: 3fr 3fr;
-    height: 50vh;
+    height: 39vh;
     width: 100%;
     margin: 0 0 20px 0;
-    gap: 50px;
+
+    @media screen and (max-width: 800px) {
+      display: block;
+      height: 75vh;
+    }
   }
 
   h3 {
@@ -39,83 +46,127 @@ const ProjectContainer = styled.section`
 
   p {
     font-size: 16px;
+    padding: 20px 30px 20px 30px;
+    border-radius: 6px;
+  }
+
+  a {
+    margin: auto;
+    margin-left: 10px;
   }
 `
 
 const StyledImg = styled.img`
+  margin: auto;
   width: 90%;
-  /* border-radius: 50%; */
+  border-radius: 6px;
 `
 
-const ListGrid = styled.section`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 0px;
-  margin: 0px;
-  /* margin-top: 50px; */
-  /* border: 1px solid;
-  border-color: ${pink}; */
-`
-
-const ListGroup = styled.ul`
-  margin-right: auto;
-  list-style: none;
-  font-size: 15px;
-  color: ${lightBlue};
-`
 const ProjectDesc = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  p {
+    background-color: ${darkBlue};
+  }
+`;
 
-const Projects = () => (
-  <ProjectContainer id="projects">
-    <div>
-      <h3 style={{ paddingLeft: "20px", color: pink }}>
-        Group-Oriented Projects :{" "}
-      </h3>
-    </div>
-    <div className="groupPGrid">
-      <ProjectDesc>
-        Mentor.me was my final project of my Coding Bootcamp. It's goal was to
-        help answer the question of what now after a student graduates a
-        bootcamp like the one I attended. The application allows users to sign
-        up and choose some topics in the world of web development they would
-        like help on and then they get matched with mentors who best represent
-        their needs as a mentee. The matching uses a basic algorithm that I
-        designed. The application allows users to make friends, join chat rooms,
-        and make posts to help out other newly grads.
-      </ProjectDesc>
+const Projects = () => {
+  const {width} = useWindowDimensions();
 
-      <StyledImg src="assets/landingpage.png" />
-    </div>
-    <div className="groupPGrid">
-        <StyledImg src='assets/postsnstuff.png' />
+  return (
+    <ProjectContainer id="projects">
+      <div>
+        <h3 style={{ paddingLeft: "20px", color: pink }}>
+          Group-Oriented Projects :
+        </h3>
+      </div>
+      <div className="groupPGrid">
+        <ProjectDesc>
+          <p>
+            Mentor.me allows users to sign up and choose some topics in the
+            world of web development they would like help on and then they get
+            matched with mentors who best represent their needs as a mentee. The
+            application allows users to make friends, join chat rooms, and make
+            posts to help out others.
+            <p style={{ padding: "0" }}>;la ndf;a</p>
+          </p>
+        </ProjectDesc>
+        <a
+          href="https://github.com/freejonm/mentor.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledImg src={mentorme} />
+        </a>
+      </div>
+      <div className="groupPGrid">
+        { width > 800 ? <>
+        <a
+          href="https://github.com/JFunck1990/Posts-n-Stuff"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledImg src={posts} />
+        </a>
 
         <ProjectDesc>
-          Posts N' Stuff is a simple social media platform where users can share
-          recipes for their favorite adult drinks or their favorite at home meal
-          recipes. Users can also post about their at home DIY projects with the
-          steps it took to complete them.
+          <p>
+            Posts N' Stuff is a simple social media platform where users can
+            share recipes for their favorite adult drinks or their favorite at
+            home meal recipes. Users can also post about their at home DIY
+            projects with the steps it took to complete them.
+            <p style={{ padding: "0" }}>;la ndf;a</p>
+          </p>
         </ProjectDesc>
-
-    </div>
-    <div className="groupPGrid">
-
+        </> : <>
         <ProjectDesc>
-          Laughs and Eats aims to solve the problem of social occasions like a
-          date or a business meeting where you don't know where to go to eat and
-          don't know what to say. With laughs eats you can enter your criteria
-          for the kind of jokes you would like to tell and some criteria for
-          food in your area. The app will then give you a list of jokes you can
-          tell your date/meeting and suggests several restaurants in your area.
+          <p>
+            Posts N' Stuff is a simple social media platform where users can
+            share recipes for their favorite adult drinks or their favorite at
+            home meal recipes. Users can also post about their at home DIY
+            projects with the steps it took to complete them.
+            <p style={{ padding: "0" }}>;la ndf;a</p>
+          </p>
         </ProjectDesc>
 
-        <StyledImg src='assets/Laughs-Eats-Screenshot.png' />
-    </div>
-  </ProjectContainer>
-)
+        <a
+          href="https://github.com/JFunck1990/Posts-n-Stuff"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledImg src={posts} />
+        </a>
+        </>
+
+        
+        
+      }
+      </div>
+      <div className="groupPGrid">
+        <ProjectDesc>
+          <p>
+            Laughs and Eats aims to solve the problem of social occasions like a
+            date or a business meeting where you don't know where to go to eat
+            and don't know what to say. With laughs eats you can enter your
+            criteria for the kind of jokes you would like to tell and some
+            criteria for food in your area. The app will then give you a list of
+            jokes you can tell your date/meeting and suggests several
+            restaurants in your area.
+            <p style={{ padding: "0" }}>;la ndf;a</p>
+          </p>
+        </ProjectDesc>
+
+        <a
+          href="https://github.com/kcrammer/Laughs-and-Eats"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledImg src={laughs} />
+        </a>
+      </div>
+    </ProjectContainer>
+  )
+}
 
 export default Projects
